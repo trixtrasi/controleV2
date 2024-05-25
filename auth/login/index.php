3efa3adb69c72cfc19aff3961e0c0122280181f4
trixtrasi <128914097+trixtrasi@users.noreply.github.com>
2024-05-25 15:38:35 -0300

    Update index.php
    alterado o caminho no head

--- auth/login/index.php
+++ auth/login/index.php
@@ -79,9 +79,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Close connection
     mysqli_close($conn);
 }
-include_once "../../public/includes/head.php";
-?>
 
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Acesso de Visitantes</title>
+    <!--FAV ICONS-->
+    <link type="image/png" rel="icon" href="../../public/img/png/password.png">
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="../../plugins/bootstrap-5.0.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../../plugins/bootstrap-5.0.2-dist/css/bootstrap-grid.css">
+    <!--CSS | COMPLEMENTAR-->
+    <link rel="stylesheet" href="../../public/css/complements.css">
+    <!--CSS | DATATABLES-->
+    <link rel="stylesheet" href="../../plugins/dataTables/css/dataTables.dataTables.min.css">
+
+    <!--BOOTSTRAP-->
+    <script src="../../plugins/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
+    <!--JQUERY-->
+    <script src="../../plugins/jquery/jquery.js"></script>
+    <!-- DATATABLES - JS -->
+    <script src="../../plugins/dataTables/js/dataTables.min.js"></script>
+</head>
 <body class="bg-dark text-light max-vh-100">
     <div class="container mt-5">
         <div class="main row d-flex justify-content-center align-items-center">
